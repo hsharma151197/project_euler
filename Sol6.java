b71f53bbbc406ef8596463ea848c85103d8a7f72
Himanshu Sharma <hsharma151197@gmail.com>
2019-02-06 19:15:30 +0530

    Add files via upload

---
+++ Sol6.java
@@ -0,0 +1,12 @@
+package project_euler;
+
+public class Sol6 {
+
+	public static void main(String[] args) {
+		//[n*(n+1)*(2*n+1)]/6
+		//[n*(n+1)]/2
+		System.out.println(((((100*101)/2)*(100*101)/2)-(100*101*201)/6));
+
+	}
+
+}
